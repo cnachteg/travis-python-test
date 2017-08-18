771c22757f3f4407ce03171b7ea1607ee6948a33
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2017-08-18 18:18:30 +0200

    Fixed errors in test code.

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -1,6 +1,7 @@
 import unittest
 import Calculator
 import math
+import sys
 
 class CalculatorTest(unittest.TestCase):
     
@@ -13,26 +14,26 @@ class CalculatorTest(unittest.TestCase):
     def test_sum(self):
         arg1 = 5
         arg2 = 3
-        assertEqual(Calculator.sum(arg1,arg2),arg1+arg2)
+        self.assertEqual(Calculator.sum(arg1,arg2),arg1+arg2)
 
     def test_sub(self):
         arg1 = 5
         arg2 = 3
-        assertEqual(Calculator.sub(arg1,arg2),arg1-arg2)
+        self.assertEqual(Calculator.sub(arg1,arg2),arg1-arg2)
 
     def test_mul(self):
         arg1 = 5
         arg2 = 3
-        assertEqual(Calculator.mul(arg1,arg2),arg1*arg2)
+        self.assertEqual(Calculator.mul(arg1,arg2),arg1*arg2)
 
     def test_div(self):
         arg1 = 5
         arg2 = 3
-        assertEqual(Calculator.div(arg1,arg2),arg1+arg2)
+        self.assertEqual(Calculator.div(arg1,arg2),arg1+arg2)
 
     def test_sqrt(self):
         arg1 = 25
-        assertEqual(Calculator.sqrt(arg1),math.sqrt(arg1))
+        self.assertEqual(Calculator.sqrt(arg1),math.sqrt(arg1))
 
 if __name__ == '__main__':
     test_suite = unittest.TestLoader().loadTestsFromTestCase(CalculatorTest)
