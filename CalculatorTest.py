8c0cb1eacd5e39ef7a924756ffcb516389379c92
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2017-08-18 18:15:48 +0200

    Corrected test code and made program exit code depend on the outcome of the tests

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -10,33 +10,39 @@ class CalculatorTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_sum():
+    def test_sum(self):
         arg1 = 5
         arg2 = 3
         assertEqual(Calculator.sum(arg1,arg2),arg1+arg2)
 
-    def test_sub():
+    def test_sub(self):
         arg1 = 5
         arg2 = 3
         assertEqual(Calculator.sub(arg1,arg2),arg1-arg2)
 
-    def test_mul():
+    def test_mul(self):
         arg1 = 5
         arg2 = 3
         assertEqual(Calculator.mul(arg1,arg2),arg1*arg2)
 
-    def test_div():
+    def test_div(self):
         arg1 = 5
         arg2 = 3
         assertEqual(Calculator.div(arg1,arg2),arg1+arg2)
 
-    def test_sqrt():
+    def test_sqrt(self):
         arg1 = 25
         assertEqual(Calculator.sqrt(arg1),math.sqrt(arg1))
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(CalculatorTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(CalculatorTest)
+    test_runner = unittest.TextTestRunner(verbosity=2).run(test_suite)
+    ret = not test_runner.wasSuccessful()
+    sys.exit(ret) 
+    """ 
+    Travis build status depends on exit code.
+    Without the last line the exit code of the program is 0 even if the tests fail.
+    """
 
 
 
