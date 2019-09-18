2071589bbfcb4b3258e8a5b60fe9714cbf5eaaf4
cnachteg <charlottenachtegael@hotmail.com>
2019-09-18 14:06:42 +0200

    Get rid of solution for testing of Travis CI

--- Calculator.py
+++ Calculator.py
@@ -6,20 +6,29 @@ class Calculator:
     @staticmethod
     def add(arg1,arg2):
         """Return the sum of arg1 and arg2"""
-        return(arg1+arg2)
+        #return(arg1+arg2)
+        pass
+
     @staticmethod
     def sub(arg1,arg2):
         """Return the difference between arg1 and arg2"""
-        return(arg1-arg2)
+        #return(arg1-arg2)
+        pass
+
     @staticmethod
     def mul(arg1,arg2):
         """Return the product of arg1 and arg2"""
-        return (arg1*arg2)
+        #return (arg1*arg2)
+        pass
+
     @staticmethod
     def div(arg1,arg2):
         """Return the division between arg1 and arg2"""
-        return (arg1/arg2)
+        #return (arg1/arg2)
+        pass
+
     @staticmethod
     def sqrt(arg1):
         """Return the square root of arg1"""
-        return math.sqrt(arg1)
+        #return math.sqrt(arg1)
+        pass
